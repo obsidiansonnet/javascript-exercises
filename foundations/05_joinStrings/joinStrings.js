4e6fa781cb45d2ead57866575417e2aed4be913c
ObsidianSonnet <216811357+obsidiansonnet@users.noreply.github.com>
2025-12-20 21:10:18 +0000

    chore: Complete test 7 of exercise 5

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -14,7 +14,8 @@ const lastName = "Stevenson";
 const thisYear = 1965;
 const birthYear = 1947;
 const fullName = firstName + " " + lastName;
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old."; 
+const age = (thisYear - birthYear);
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."; 
 
 
 
