38fba5a30f9a48d8836144a445738704a865e731
ObsidianSonnet <216811357+obsidiansonnet@users.noreply.github.com>
2026-03-22 22:14:24 +0000

    chore: Complete exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...argument) {
+    for (let i = 0; i < argument.length; i++) {
+        for (let j = array.length - 1; j >= 0; j--) {
+            if (argument[i] === array[j]) {
+                array.splice(j, 1)
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
