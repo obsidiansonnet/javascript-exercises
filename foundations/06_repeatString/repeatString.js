369c88915c3d54169f45766feeb96c9823cd4370
ObsidianSonnet <216811357+obsidiansonnet@users.noreply.github.com>
2026-03-22 01:32:31 +0000

    chore: Complete exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return "ERROR";
+      }
+      let sanitizedString = string;
+      if (typeof string !== "string") {
+        sanitizedString = string.toString();
+      }
 
+      let finalOutcome = "";
+      for (let i = 1; i<=number; i++){
+        finalOutcome += sanitizedString;
+      }
+      return finalOutcome;
 };
 
 // Do not edit below this line
