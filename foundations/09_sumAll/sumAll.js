15a1b8e9c16895d93fbef4cbe389f00935bc6d93
ObsidianSonnet <216811357+obsidiansonnet@users.noreply.github.com>
2026-03-25 19:55:54 +0000

    chore: Complete exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 1 || b < 1) return "ERROR";
+    const lowerEnd = Math.min(a, b);
+    const upperEnd = Math.max(a, b);
+    const numberOfValues = (upperEnd - lowerEnd) + 1;
+    return (numberOfValues * (lowerEnd + upperEnd) / 2);
 };
 
 // Do not edit below this line
