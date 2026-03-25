a12eb8ed4dc5a98c8d65efcaa46b81e20493643a
ObsidianSonnet <216811357+obsidiansonnet@users.noreply.github.com>
2026-03-25 21:13:41 +0000

    chore: Complete exercise 10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (year <= 0 || !Number.isInteger(year)) return false;
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
 
 // Do not edit below this line
