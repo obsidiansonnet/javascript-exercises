803dffe106292b390e8dcd1d378bf52c0d0bf3cd
ObsidianSonnet <216811357+obsidiansonnet@users.noreply.github.com>
2026-03-26 02:20:15 +0000

    chore: Complete exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInF) {
+  const rawCelsius = (tempInF - 32) * 5 / 9
+  return +(rawCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInC) {
+  const rawFahrenheit = (tempInC * 9 / 5) + 32 
+  return +(rawFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
