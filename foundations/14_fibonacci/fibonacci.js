2978aa7e46c5f5b18b754dc8c98799d3e84df2dd
ObsidianSonnet <216811357+obsidiansonnet@users.noreply.github.com>
2026-05-22 18:17:08 +0000

    chore: Complete exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let sanitizedInput = +n;
 
+    let zeroth = 0;
+    let first = 1;
+    let nth;
+
+    if (sanitizedInput === 0) { return 0; }
+    if (sanitizedInput === 1) { return first; }
+    if (sanitizedInput < 0) { return "OOPS"; }
+
+    for (let i = 0; i <= (sanitizedInput - 2); i++) {
+        nth = zeroth + first;
+        zeroth = first;
+        first = nth;
+    }
+    return nth;
 };
 
 // Do not edit below this line
