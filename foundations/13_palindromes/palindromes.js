468a01c1eb718fda2f436dd3777e7fa4dd564ea9
ObsidianSonnet <216811357+obsidiansonnet@users.noreply.github.com>
2026-05-22 17:08:12 +0000

    chore: Complete exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let sanitizedString = string.toLowerCase().replaceAll(" ", "")
+    for (let i = 0, j = sanitizedString.length - 1; i <= j;) {
+        if ((sanitizedString.charCodeAt(i) < 97 || sanitizedString.charCodeAt(i) > 122) && (sanitizedString.charCodeAt(i) < 48 || sanitizedString.charCodeAt(i) > 57)) {
+            i++;
+        }
+        else if ((sanitizedString.charCodeAt(j) < 97 || sanitizedString.charCodeAt(j) > 122) && (sanitizedString.charCodeAt(j) < 48 || sanitizedString.charCodeAt(j) > 57)) {
+            j--;
+        }
+        else if (sanitizedString[i] !== sanitizedString[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true
 };
 
 // Do not edit below this line
