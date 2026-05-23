006c40e5f12ec79ea8d58904aeb4f2b070496c9c
ObsidianSonnet <216811357+obsidiansonnet@users.noreply.github.com>
2026-05-23 09:04:28 +0000

    chore: Complete exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+    let currentYear = new Date().getFullYear();
 
+    function getAge(object) {
+        let age = object.yearOfDeath === undefined ? currentYear - object.yearOfBirth : object.yearOfDeath - object.yearOfBirth;
+        return age;
+    }
+
+    let oldestPersonObject = array.reduce(function (accumulator, person) {
+        let ageAccumulator = getAge(accumulator);
+        let agePerson = getAge(person);
+
+        if (ageAccumulator > agePerson) {
+            return accumulator;
+        }
+
+        else { return person };
+    });  
+    return oldestPersonObject;
 };
 
 // Do not edit below this line
